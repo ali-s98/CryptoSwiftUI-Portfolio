b645115b4412609dccf763bff830c527b3aa4fa9
ali.sadeghi <ali.sadeghiii1998@gmail.com>
2023-10-22 13:03:29 +0330

    Initial Commit

---
+++ CryptoSwiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CryptoSwiftui
+//
+//  Created by ali sadeghi on 30/07/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoSwiftui/CryptoSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftuiApp.swift
+//  CryptoSwiftui
+//
+//  Created by ali sadeghi on 30/07/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
